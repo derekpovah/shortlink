79c531b8468630a373da2c9e118434c0987f7d6b
Derek <derek@derekpovah.com>
2018-06-13 09:54:47 -0700

    removed trailing whitespace

--- app/models/concerns/url_validator.rb
+++ app/models/concerns/url_validator.rb
@@ -3,7 +3,7 @@ class UrlValidator < ActiveModel::EachValidator
     record.errors[attribute] << (options[:message] || 'must be a valid URL') unless url_valid?(value)
   end
 
-  def url_valid?(url) 
+  def url_valid?(url)
     uri = URI.parse(url) rescue false
     return false unless uri.kind_of?(URI::HTTP) || uri.kind_of?(URI::HTTPS) 
     PublicSuffix.valid?(url.split('/').last, default_rule: nil)
